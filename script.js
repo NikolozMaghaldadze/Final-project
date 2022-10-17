bb77d14a93ddf65fe5288246da732b255dc61fa1
NikaMaghaldadze <nikamaghaldadze13@gmail.com>
2022-10-17 21:34:12 +0400

    adding elements+responsive

---
+++ script.js
@@ -0,0 +1,11 @@
+document.getElementById("alert-button").addEventListener("click" , function(){
+    alert("you pressed this button");
+});
+
+let navBar = document.getElementById('navBar');
+let toggleBar = document.getElementById('toggleBar');
+
+
+toggleBar.addEventListener("click", function(){
+    navBar.classList.toggle("navActive");
+})
